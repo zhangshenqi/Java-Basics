9aff91fe6397f807dd1ccf897345b68e61add6cb
zhangshenqi <shenqiz1995@gmail.com>
2021-03-14 22:04:37 -0400

    Add the ArrayList class and testing.

---
+++ data_structures/list/ArrayList.java
@@ -0,0 +1,259 @@
+import java.util.Arrays;
+
+/**
+ * Resizable-array implementation of the List interface.
+ * @author Shenqi Zhang
+ * @param <E> the type of elements in this list
+ *
+ */
+public class ArrayList<E> implements List<E> {
+    /**
+     * Default capacity.
+     */
+    private static final int DEFAULT_INITIAL_CAPACITY = 10;
+    /**
+     * Elements in this list.
+     */
+    private Object[] elements;
+    /**
+     * size of this list.
+     */
+    private int size;
+    
+    /**
+     * Constructs an empty list with an initial capacity of ten.
+     */
+    public ArrayList() {
+        this(DEFAULT_INITIAL_CAPACITY);
+    }
+    
+    /**
+     * Constructs an empty list with the specified initial capacity.
+     * @param initialCapacity the initial capacity of the list
+     */
+    public ArrayList(int initialCapacity) {
+        elements = new Object[initialCapacity];
+    }
+    
+    /**
+     * Returns the number of elements in this list.
+     * @return the number of elements in this list
+     */
+    public int size() {
+        return size;
+    }
+    
+    /**
+     * Returns true if this list contains no elements.
+     * @return if this list contains no elements
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    
+    /**
+     * Returns the capacity of this list.
+     * @return the capacity of this list
+     */
+    public int capacity() {
+        return elements.length;
+    }
+    
+    /**
+     * Returns true if this list contains the specified element.
+     * @param e element whose presence in this list is to be tested
+     * @return true if this list contains the specified element
+     */
+    public boolean contains(E e) {
+        return indexOf(e) != -1;
+    }
+    
+    /**
+     * Appends the specified element to the end of this list (optional operation).
+     * @param e element to be appended to this list
+     * @return true if this list changed as a result of the call
+     */
+    public boolean add(E e) {
+        ensureCapacity(size + 1);
+        elements[size] = e;
+        size++;
+        return true;
+    }
+    
+    /**
+     * Removes the first occurrence of the specified element from this list,
+     * if it is present (optional operation).
+     * @param e element to be removed from this list, if present
+     * @return true if this list contained the specified element
+     */
+    public boolean remove(E e) {
+        int index = indexOf(e);
+        if (index == -1) {
+            return false;
+        }
+        for (int i = index + 1; i < size; i++) {
+            elements[i - 1] = elements[i];
+        }
+        elements[size - 1] = null;
+        size--;
+        return true;
+    }
+    
+    /**
+     * Removes all of the elements from this list (optional operation).
+     */
+    public void clear() {
+        Arrays.fill(elements, 0, size, null);
+        size = 0;
+    }
+    
+    /**
+     * Returns the element at the specified position in this list.
+     * @param index index of the element to return
+     * @return the element at the specified position in this list
+     */
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        @SuppressWarnings("unchecked")
+        E e  = (E) elements[index];
+        return e;
+    }
+    
+    /**
+     * Replaces the element at the specified position in this list with the specified element (optional operation).
+     * @param index index of the element to replace
+     * @param element element to be stored at the specified position
+     * @return the element previously at the specified position
+     */
+    public E set(int index, E element) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        @SuppressWarnings("unchecked")
+        E e  = (E) elements[index];
+        elements[index] = element;
+        return e;
+    }
+    
+    /**
+     * Inserts the specified element at the specified position in this list (optional operation).
+     * @param index index at which the specified element is to be inserted
+     * @param element element to be inserted
+     */
+    public void add(int index, E element) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        ensureCapacity(size + 1);
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+        elements[index] = element;
+        size++;
+    }
+    
+    /**
+     * Removes the element at the specified position in this list (optional operation).
+     * @param index the index of the element to be removed
+     * @return the element previously at the specified position
+     */
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        @SuppressWarnings("unchecked")
+        E e  = (E) elements[index];
+        for (int i = index + 1; i < size; i++) {
+            elements[i - 1] = elements[i];
+        }
+        elements[size - 1] = null;
+        size--;
+        return e;
+    }
+    
+    /**
+     * Returns the index of the first occurrence of the specified element in this list,
+     * or -1 if this list does not contain the element.
+     * @param e element to search for
+     * @return the index of the first occurrence of the specified element in this list, 
+     * or -1 if this list does not contain the element
+     */
+    public int indexOf(E e) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i] == null) {
+                if (e == null) {
+                    return i;
+                }
+            } else {
+                if (elements[i].equals(e)) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+    
+    /**
+     * Returns the index of the last occurrence of the specified element in this list,
+     * or -1 if this list does not contain the element.
+     * @param e element to search for
+     * @return the index of the last occurrence of the specified element in this list,
+     * or -1 if this list does not contain the element
+     */
+    public int lastIndexOf(E e) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (elements[i] == null) {
+                if (e == null) {
+                    return i;
+                }
+            } else {
+                if (elements[i].equals(e)) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+    
+    /**
+     * Trims the capacity of this ArrayList instance to be the list's current size.
+     */
+    public void trimToSize() {
+        elements = Arrays.copyOf(elements, size);
+    }
+    
+    /**
+     * Increases the capacity of this ArrayList instance, if necessary,
+     * to ensure that it can hold at least the number of elements specified by the minimum capacity argument.
+     * @param minCapacity the desired minimum capacity
+     */
+    public void ensureCapacity(int minCapacity) {
+        int oldCapacity = elements.length;
+        if (minCapacity > oldCapacity) {
+            int newCapacity = (oldCapacity * 3) / 2 + 1;
+            if (newCapacity < minCapacity) {
+                newCapacity = minCapacity;
+            }
+            elements = Arrays.copyOf(elements, newCapacity);
+        }
+    }
+    
+    /**
+     * Returns a string representation of this list.
+     * @return a string representation of this list
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        if (size > 0) {
+            sb.append(elements[0]);
+            for (int i = 1; i < size; i++) {
+                sb.append(", ").append(elements[i]);
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ data_structures/list/ArrayListTest.java
@@ -0,0 +1,94 @@
+/**
+ * Code for testing ArrayList.
+ * @author Shenqi Zhang
+ *
+ */
+public class ArrayListTest {
+    public static <E> void printList(List<E> list) {
+        System.out.print("size = ");
+        System.out.print(list.size());
+        if (list instanceof ArrayList<?>) {
+            System.out.print(" capacity = ");
+            System.out.print(((ArrayList<E>) list).capacity());
+        }
+        System.out.print(" elements = ");
+        System.out.println(list);
+    }
+    
+    /**
+     * Test ArrayList
+     * @param args arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Create a list");
+        List<Integer> list1 = new ArrayList<Integer>();
+        printList(list1);
+        System.out.println("The list is empty");
+        System.out.println(list1.isEmpty());
+        System.out.println("Add elements");
+        list1.add(1);
+        list1.add(3);
+        list1.add(5);
+        list1.add(7);
+        list1.add(9);
+        printList(list1);
+        System.out.println("Add elements");
+        list1.add(0, 0);
+        list1.add(2, 2);
+        list1.add(4, 4);
+        list1.add(6, 6);
+        list1.add(8, 8);
+        printList(list1);
+        System.out.println("Add elements");
+        list1.add(11);
+        System.out.println("Add elements");
+        list1.add(12);
+        list1.add(13);
+        list1.add(15);
+        list1.add(10, 10);
+        list1.add(15, 16);
+        printList(list1);
+        System.out.println("Add elements");
+        list1.add(14, 14);
+        printList(list1);
+        System.out.println("The list is empty");
+        System.out.println(list1.isEmpty());
+        System.out.println("The list contains 8");
+        System.out.println(list1.contains(8));
+        System.out.println("The list contains 8");
+        System.out.println(list1.contains(18));
+        System.out.println("Get the index of 8");
+        System.out.println(list1.indexOf(8));
+        System.out.println("Get the index of 18");
+        System.out.println(list1.indexOf(18));
+        System.out.println("Get the last index of 8");
+        System.out.println(list1.lastIndexOf(8));
+        System.out.println("Get the last index of 18");
+        System.out.println(list1.lastIndexOf(18));
+        System.out.println("Get the element at index 8");
+        System.out.println(list1.get(8));
+        System.out.println("Set the element at index 8 to be 18");
+        System.out.println(list1.set(8, 18));
+        printList(list1);
+        System.out.println("Clear the list");
+        list1.clear();
+        printList(list1);
+        System.out.println();
+        
+        System.out.println("Create a list");
+        List<String> list2 = new ArrayList<String>(3);
+        printList(list2);
+        System.out.println("Add elements");
+        list2.add("One");
+        list2.add("Two");
+        list2.add(0, "Zero");
+        list2.add(3, "Three");
+        printList(list2);
+        System.out.println("Remove Zero");
+        System.out.println(list2.remove("Zero"));
+        printList(list2);
+        System.out.println("Remove element at index 1");
+        System.out.println(list2.remove(1));
+        printList(list2);
+    }
+}
