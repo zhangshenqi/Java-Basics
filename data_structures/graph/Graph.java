606ba15e2e69d88564ec6c39959ceb5dd9a33c61
zhangshenqi <shenqiz1995@gmail.com>
2021-03-23 21:16:27 -0400

    Add the graph interface.

---
+++ data_structures/graph/Graph.java
@@ -0,0 +1,39 @@
+import java.util.List;
+
+/**
+ * A graph consists of a finite set of vertices and edges.
+ * @author Shenqi Zhang
+ *
+ */
+public interface Graph {
+    /**
+     * Adds a vertex into this graph.
+     * @param vertex the vertex to be added
+     * @return true if this graph changed as a result of the call
+     */
+    boolean addVertex(int vertex);
+    
+    /**
+     * Removes a vertex from this graph.
+     * @param vertex the vertex to be removed
+     * @return true if this graph changed as a result of the call
+     */
+    boolean removeVertex(int vertex);
+    
+    /**
+     * Gets the shortest distance from the source vertex to the destination vertex.
+     * @param source the source vertex
+     * @param destination the destination vertex
+     * @return the shortest distance from the source vertex to the destination vertex
+     */
+    int getShortestDistance(int source, int destination);
+    
+    /**
+     * Gets the shortest path from the source vertex to the destination vertex.
+     * If there are multiple shortest path, there is no guarantee which one will be found.
+     * @param source the source vertex
+     * @param destination the destination vertex
+     * @return the shortest path from the source vertex to the destination vertex
+     */
+    List<Integer> getShortestPath(int source, int destination);
+}
