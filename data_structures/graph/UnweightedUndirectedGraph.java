be2afda4928af426d5f760b1f2cb05e6f8393177
zhangshenqi <shenqiz1995@gmail.com>
2021-03-24 21:54:36 -0400

    Add containsLoop in unweighted undirected graph.

--- data_structures/graph/UnweightedUndirectedGraph.java
+++ data_structures/graph/UnweightedUndirectedGraph.java
@@ -1,3 +1,8 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Map;
+import java.util.HashMap;
+
 /**
  * Class for unweighted undirected graph.
  * @author Shenqi Zhang
@@ -39,5 +44,38 @@ public class UnweightedUndirectedGraph extends UnweightedGraph {
         adjacencyList.get(source).remove(Integer.valueOf(destination));
         adjacencyList.get(destination).remove(Integer.valueOf(source));
         return true;
+    }
+    
+    /**
+     * Returns true if this graph contains loop.
+     * @return true if this graph contains loop
+     */
+    @Override
+    public boolean containsLoop() {
+        Map<Integer, Integer> degrees = new HashMap<Integer, Integer>();
+        Queue<Integer> queue = new LinkedList<Integer>();
+        for (int key : adjacencyList.keySet()) {
+            int degree = adjacencyList.get(key).size();
+            if (degree > 0) {
+                degrees.put(key, degree);
+                if (degree == 1) {
+                    queue.add(key);
+                }
+            }
+        }
+        
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            degrees.remove(curr);
+            for (int neighbor : adjacencyList.get(curr)) {
+                if (degrees.containsKey(neighbor)) {
+                    degrees.put(neighbor, degrees.get(neighbor) - 1);
+                    if (degrees.get(neighbor) == 1) {
+                        queue.add(neighbor);
+                    }
+                }
+            }
+        }
+        return degrees.size() != 0;
     }
 }
