71551869eb442d072ef97c6dfb30c8239e8c3eed
zhangshenqi <shenqiz1995@gmail.com>
2021-03-25 19:00:17 -0400

    Add find shortest distance and path for weighted graph.

--- data_structures/graph/WeightedGraph.java
+++ data_structures/graph/WeightedGraph.java
@@ -1,5 +1,8 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -77,16 +80,119 @@ public abstract class WeightedGraph implements Graph {
      */
     public abstract boolean removeEdge(int source, int destination);
     
+    /**
+     * Gets the shortest distance from the source vertex to the destination vertex.
+     * @param source the source vertex
+     * @param destination the destination vertex
+     * @return the shortest distance from the source vertex to the destination vertex, if it is found; otherwise, -1.
+     */
     @Override
     public int getShortestDistance(int source, int destination) {
-        // TODO Auto-generated method stub
-        return 0;
+        if (!adjacencyList.containsKey(source) || !adjacencyList.containsKey(destination)) {
+            return -1;
+        }
+        
+        Map<Integer, Integer> distances = new HashMap<Integer, Integer>();
+        for (int key : adjacencyList.keySet()) {
+            distances.put(key, Integer.MAX_VALUE);
+        }
+        distances.put(source, 0);
+        Set<Integer> settled = new HashSet<Integer>();
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> (a[1] - b[1]));
+        pq.add(new int[] {source, 0});
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            if (curr[0] == destination) {
+                return curr[1];
+            }
+            if (!settled.add(curr[0])) {
+                continue;
+            }
+            List<Pair> neighbors = adjacencyList.get(curr[0]);
+            for (Pair neighbor : neighbors) {
+                if (settled.contains(neighbor.vertex)) {
+                    continue;
+                }
+                int newDistance = distances.get(curr[0]) + neighbor.weight;
+                if (newDistance < distances.get(neighbor.vertex)) {
+                    distances.put(neighbor.vertex, newDistance);
+                    pq.add(new int[] {neighbor.vertex, newDistance});
+                }
+            }
+        }
+        return -1;
     }
     
+    /**
+     * Gets the shortest path from the source vertex to the destination vertex.
+     * If there are multiple shortest path, there is no guarantee which one will be found.
+     * @param source the source vertex
+     * @param destination the destination vertex
+     * @return the shortest path from the source vertex to the destination vertex, if it is found; otherwise, empty list.
+     */
     @Override
     public List<Integer> getShortestPath(int source, int destination) {
-        // TODO Auto-generated method stub
-        return null;
+        List<Integer> path = new ArrayList<Integer>();
+        if (!adjacencyList.containsKey(source) || !adjacencyList.containsKey(destination)) {
+            return path;
+        }
+        
+        Map<Integer, Integer> distances = new HashMap<Integer, Integer>();
+        for (int key : adjacencyList.keySet()) {
+            distances.put(key, Integer.MAX_VALUE);
+        }
+        distances.put(source, 0);
+        Set<Integer> settled = new HashSet<Integer>();
+        Map<Integer, Integer> parentMap = new HashMap<Integer, Integer>();
+        parentMap.put(source, source);
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> (a[1] - b[1]));
+        pq.add(new int[] {source, 0});
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            if (curr[0] == destination) {
+                retrievePath(destination, parentMap, path);
+                return path;
+            }
+            if (!settled.add(curr[0])) {
+                continue;
+            }
+            List<Pair> neighbors = adjacencyList.get(curr[0]);
+            for (Pair neighbor : neighbors) {
+                if (settled.contains(neighbor.vertex)) {
+                    continue;
+                }
+                int newDistance = distances.get(curr[0]) + neighbor.weight;
+                if (newDistance < distances.get(neighbor.vertex)) {
+                    distances.put(neighbor.vertex, newDistance);
+                    parentMap.put(neighbor.vertex, curr[0]);
+                    pq.add(new int[] {neighbor.vertex, newDistance});
+                }
+            }
+        }
+        return path;
+    }
+    
+    /**
+     * Retrieves the path from the source to the current vertex.
+     * @param curr the current vertex
+     * @param parentMap map which stores the parent of each vertex
+     * @param path path
+     */
+    private void retrievePath(int curr, Map<Integer, Integer> parentMap, List<Integer> path) {
+        while (parentMap.get(curr) != curr) {
+            path.add(curr);
+            curr = parentMap.get(curr);
+        }
+        path.add(curr);
+        
+        int i = 0, j = path.size() - 1;
+        while (i < j) {
+            int temp = path.get(i);
+            path.set(i, path.get(j));
+            path.set(j, temp);
+            i++;
+            j--;
+        }
     }
     
     /**
