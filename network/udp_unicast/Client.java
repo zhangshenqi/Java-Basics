a02c78f4df5d6e1aa6ea6fd52bb3a8223439b117
zhangshenqi <shenqiz1995@gmail.com>
2021-03-28 23:07:16 -0400

    Add receiving response for the client.

--- network/udp_unicast/Client.java
+++ network/udp_unicast/Client.java
@@ -6,28 +6,55 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
+/**
+ * A unicast UDP client.
+ * @author Shenqi Zhang
+ *
+ */
 public class Client {
-
+    /**
+     * Prints the log.
+     * @param sendRequest true if this is for sending a request
+     * @param packet packet
+     * @param message message
+     */
+    public static void printLog(boolean sendRequest, DatagramPacket packet, String message) {
+        StringBuilder sb = new StringBuilder();
+        if (sendRequest) {
+            sb.append("Send request").append("\n");
+        } else {
+            sb.append("Receive response").append("\n");
+        }
+        sb.append("Time: ").append(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS").format(new Date())).append("\n");
+        sb.append("Server: ").append(packet.getAddress().getHostAddress()).append(':').append(packet.getPort()).append("\n");
+        sb.append("Message: ").append(message).append("\n");
+        System.out.println(sb.toString());
+    }
+    
+    /**
+     * A shell which sends user-specified requests and receives responses.
+     * @param args arguments
+     */
     public static void main(String[] args) {
-        DatagramSocket socket = null;
         Scanner scanner = null;
+        DatagramSocket socket = null;
         try {
             InetAddress address = InetAddress.getByName("localhost");
-            socket = new DatagramSocket();
             scanner = new Scanner(System.in);
+            socket = new DatagramSocket();
             while (true) {
+                System.out.print("Please Enter your request: ");
                 String request = scanner.next();
-                if (request.trim().toLowerCase().equals("kill")) {
-                    break;
-                }
                 byte[] buf = request.getBytes();
                 DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 7077);
-                StringBuilder sb =  new StringBuilder("Send Request").append("\n");
-                sb.append("Time: ").append(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS").format(new Date())).append("\n");
-                sb.append("Server: ").append(packet.getAddress().getHostAddress()).append(':').append(packet.getPort()).append("\n");
-                sb.append("Message: ").append(request).append("\n");
-                System.out.println(sb.toString());
+                printLog(true, packet, request);
                 socket.send(packet);
+                
+                buf = new byte[100];
+                packet.setData(buf);
+                socket.receive(packet);
+                String response = new String(packet.getData());
+                printLog(false, packet, response);
             }
         } catch (IOException e) {
             e.printStackTrace();
